ad6f27980d2f748f0e6010e1681e1cf21a6d69bb
nicompile <nicompile@tailwindmail.com>
2025-05-26 17:55:17 -0700

    serverless deployable

---
+++ deployables/ics-proxy/ics-proxy.go
@@ -0,0 +1,47 @@
+package icsproxy
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/nicompile/ics-proxy/internal/config"
+	"github.com/nicompile/ics-proxy/internal/parser"
+	"github.com/nicompile/infra-library-go/pkg/serverlessapi"
+)
+
+func proxy(request serverlessapi.Request) serverlessapi.Response {
+	conf := config.Get()
+	resp, err := http.Get(conf.Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := strings.Builder{}
+	p := parser.New(resp.Body, &body)
+	err = p.Parse()
+	if err != nil {
+		panic(err)
+	}
+
+	return serverlessapi.Response{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "text/calendar"},
+		Body:       body.String(),
+	}
+}
+
+func GetEndpoints() []serverlessapi.Endpoint {
+
+	return []serverlessapi.Endpoint{
+		{
+			Method: "GET",
+			Path:   "/",
+			Target: proxy,
+			Timeout: serverlessapi.Timeout{
+				Minutes: 0,
+				Seconds: 30,
+			},
+		},
+	}
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/nicompile/ics-proxy
 
-go 1.18
+go 1.22.2
 
-require github.com/aws/aws-lambda-go v1.48.0
+require (
+	github.com/aws/aws-lambda-go v1.48.0
+	github.com/nicompile/infra-library-go v0.0.0-20250527004304-466d9e0fb0cd
+)
